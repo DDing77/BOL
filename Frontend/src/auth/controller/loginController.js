520f22016f022cc9b940e53db1ff427252d9fa18
DDing77 <smg76262585@gmail.com>
2021-11-10 01:20:48 +0900

    Add: 로그아웃컨트롤러 추가

--- Frontend/src/auth/controller/loginController.js
+++ Frontend/src/auth/controller/loginController.js
@@ -1,33 +1,47 @@
-import react from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 // 로그인 요청
 export const onLoginHandler = (data) => {
-        console.log("핸들러전달")
-        console.log(data);
+  console.log("핸들러전달");
+  console.log(data);
 
-        console.log('click login')
-        console.log('Email : ', data.Email)
-        console.log('PW : ', data.Password)
+  console.log("click login");
+  console.log("Email : ", data.Email);
+  console.log("PW : ", data.Password);
 
-        let body = {
-            email: data.Email,
-            password: data.Password
-        }
+  let body = {
+    email: data.Email,
+    password: data.Password,
+  };
 
-        axios.post("/api/users/login", body)
-        .then((res) => {
-          console.log(res);
-          console.log("res.data.email :: ", res.data.email);
-          console.log("res.data.password :: ", res.data.password);
-          if (res.data.email === body.email) {
-            console.log("====================로그인 성공!");
-            sessionStorage.setItem("user", JSON.stringify(true));
-            // props.history.push(`/personal/${res.data.id}`);
-            window.location.reload();
-          } else {
-            alert("Login error");
-          }
-        })
-        .catch();
-}
+  axios
+    .post("/api/users/login", body)
+    .then((res) => {
+      console.log(res);
+      console.log("res.data.email :: ", res.data.email);
+      console.log("res.data.password :: ", res.data.password);
+      if (res.data.email === body.email) {
+        console.log("====================로그인 성공!");
+        sessionStorage.setItem("user", JSON.stringify(true));
+        // props.history.push(`/personal/${res.data.id}`);
+        window.location.reload();
+      } else {
+        alert("Login error");
+      }
+    })
+    .catch();
+};
+
+// 로그아웃
+export const Logout = () => {
+  axios.get("/api/users/logout").then((req, res) => {
+    console.log("===로그아웃 시작==");
+    console.log(req.data);
+    if (req.data) {
+      console.log("============세션 지우기 성공");
+      sessionStorage.clear();
+      window.location.href = "/";
+    }
+  });
+};
