1df87d02f00a672606fd2ae42ddeec04710da929
DDing77 <smg76262585@gmail.com>
2021-11-10 00:28:12 +0900

    feat: 라우팅경로 추가

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import LoginPage from "./page/loginPage";
+import MainPage from "./page/mainPage";
+import GameMakePage from "./page/gameMakePage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      App
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/make" element={<GameMakePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
