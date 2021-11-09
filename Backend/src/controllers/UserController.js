1b7197cc1bb91a266e45d08a6b9e8d75901351f2
DDing77 <smg76262585@gmail.com>
2021-11-10 00:40:25 +0900

    Add: login

--- Backend/src/controllers/UserController.js
+++ Backend/src/controllers/UserController.js
@@ -5,5 +5,6 @@ import * as UserService from '../services/UserService';
 const router = express.Router();
 
 router.post('/', AuthHander.isNotLoggined, UserService.SignUp);
+router.post('/login', AuthHander.isNotLoggined, UserService.Login);
 router.get('/alluser', UserService.UserInfo);
 export default router;
