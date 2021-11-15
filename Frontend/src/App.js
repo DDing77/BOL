9426283d8c26074069e96852ec744df820097873
DDing77 <smg76262585@gmail.com>
2021-11-15 15:39:27 +0900

    feat: request profile to sever, checking authentication

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,43 @@
 import "./App.css";
-import React from "react";
+import React, {useEffect} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import IsLogin from "./auth/isLogin";
+import isLogin from "./auth/isLogin";
+import {loginCheck} from "./store/actions/user_action";
 
 import MainHeader from "./components/mainHeader";
-import LoginPage from "./page/loginPage";
 import MainPage from "./page/mainPage";
+import LoginPage from "./page/loginPage";
+import RegisterPage from "./page/registerPage";
 import GameMakePage from "./page/gameMakePage";
+
 function App() {
+  const dispatch = useDispatch();
+  const authenticated = isLogin();
+  const isLoginState = useSelector((store) => store.user);
+  // 현재 사용자가 로그인한 상태인지 확인
+  console.log(isLoginState);
+  console.log("app");
+  console.log("auth : " + authenticated);
+  console.log("isLogin : " + IsLogin());
+  console.log(
+    "sessionStorage.user : " + JSON.parse(sessionStorage.getItem("user"))
+  );
+  console.log(sessionStorage)
+  useEffect(() => {
+		if (authenticated) { // 세션에 유저정보가 있으면 진행
+			dispatch(loginCheck()); // 세션에 있는 유저의 프로필 정보를 가져옴
+		}
+	}, []);
+
   return (
     <BrowserRouter>
-      <MainHeader/>
+      <MainHeader />
       <Routes>
         <Route exact path="/" element={<MainPage />} />
         <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage/>} />
         <Route exact path="/make" element={<GameMakePage />} />
       </Routes>
     </BrowserRouter>
