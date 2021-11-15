80c995da02173ede6b5337e0069c126917c75048
DDing77 <smg76262585@gmail.com>
2021-11-15 20:40:47 +0900

    fix: 프로필요청 로직변경

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import IsLogin from "./auth/isLogin";
 import isLogin from "./auth/isLogin";
-import {loginCheck} from "./store/actions/user_action";
+import { loginCheck } from "./store/actions/user_action";
 
 import MainHeader from "./components/mainHeader";
 import MainPage from "./page/mainPage";
@@ -14,9 +14,24 @@ import GameMakePage from "./page/gameMakePage";
 
 function App() {
   const dispatch = useDispatch();
-  const authenticated = isLogin();
+
   const isLoginState = useSelector((store) => store.user);
   // 현재 사용자가 로그인한 상태인지 확인
+  let authenticated = isLogin();
+
+  useEffect(() => {
+    // if (authenticated == false && isLoginState.isLogin == true) {
+    // 세션에 유저정보가 있으면 진행
+    authenticated = isLogin();
+    if (authenticated) {
+      console.log("프로필요청");
+      dispatch(loginCheck()); // 세션에 있는 유저의 프로필 정보를 가져옴
+    }
+    // } else if (authenticated == true && isLogin.isLogin == false) {
+
+    // }
+  }, [isLoginState.isLogin]);
+
   console.log(isLoginState);
   console.log("app");
   console.log("auth : " + authenticated);
@@ -24,20 +39,14 @@ function App() {
   console.log(
     "sessionStorage.user : " + JSON.parse(sessionStorage.getItem("user"))
   );
-  console.log(sessionStorage)
-  useEffect(() => {
-		if (authenticated) { // 세션에 유저정보가 있으면 진행
-			dispatch(loginCheck()); // 세션에 있는 유저의 프로필 정보를 가져옴
-		}
-	}, []);
-
+  console.log(sessionStorage);
   return (
     <BrowserRouter>
       <MainHeader />
       <Routes>
         <Route exact path="/" element={<MainPage />} />
         <Route exact path="/login" element={<LoginPage />} />
-        <Route exact path="/register" element={<RegisterPage/>} />
+        <Route exact path="/register" element={<RegisterPage />} />
         <Route exact path="/make" element={<GameMakePage />} />
       </Routes>
     </BrowserRouter>
