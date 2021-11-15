f2c15177ecb0eecea083fef20b75acecdff2c90c
DDing77 <smg76262585@gmail.com>
2021-11-15 23:02:07 +0900

    refactor: Login 리팩토링

--- Frontend/src/auth/controller/userController.js
+++ Frontend/src/auth/controller/userController.js
@@ -1,50 +1,44 @@
-import axios from "axios";
-import { logoutUser, registerUser } from "../../store/actions/user_action";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../../store/actions/user_action";
 
-
-// 로그인 요청 => 리펙토링하기
-export const onLoginHandler = (data) => {
-  console.log("핸들러전달");
-  console.log(data);
-
-  console.log("click login");
-  console.log("Email : ", data.Email);
-  console.log("PW : ", data.Password);
+// 로그인 요청 
+export const Login = (dispath, navigate, data) => {
+  console.log("===로그인 시도===");
 
   let body = {
     email: data.Email,
     password: data.Password,
   };
 
-  axios
-    .post("/api/users/login", body)
+  dispath(loginUser(body))
     .then((res) => {
       console.log(res);
-      console.log("res.data.email :: ", res.data.email);
-      console.log("res.data.password :: ", res.data.password);
-      if (res.data.email === body.email) {
-        console.log("====================로그인 성공!");
+      if (res.payload.success) {
+        console.log("===로그인 성공===");
         sessionStorage.setItem("user", JSON.stringify(true));
-        // // props.history.push(`/personal/${res.data.id}`);
-        // window.location.href = "/";
       } else {
-        alert("Login error");
+        alert("===로그인 실패===");
       }
+      navigate("/");
     })
     .catch();
 };
 
+
 // 로그아웃
-export const Logout = (dispath, navigate ) => {
+export const Logout = (dispath, navigate) => {
   console.log("===로그아웃 시도===");
-  dispath(logoutUser()).then(req => {
+  dispath(logoutUser()).then((req) => {
     console.log(req);
-    if(req.payload.success) {
+    if (req.payload.success) {
       console.log("===로그아웃 성공===");
       sessionStorage.clear();
     }
     navigate("/");
-  })
+  });
 };
 
 // 회원가입
@@ -54,14 +48,14 @@ export const Register = (dispath, navigate, data) => {
   let body = {
     email: data.email,
     name: data.name,
-    password: data.password
-  }
-  console.log(body)
-  dispath(registerUser(body)).then(res => {
-    console.log(res);
-    if(res.payload.registerSuccess) {
+    password: data.password,
+  };
+
+  dispath(registerUser(body)).then((res) => {
+    console.log(res.payload);
+    if (res.payload != null) {
       console.log("===회원가입 성공===");
     }
     navigate("/");
-  })
-}
+  });
+};
