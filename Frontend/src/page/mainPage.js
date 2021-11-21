de3f9568290b11af8707c6a67a8b8534ddeebaf9
DDing77 <smg76262585@gmail.com>
2021-11-22 08:22:31 +0900

    Feat: mainPage Skeletion

--- Frontend/src/page/mainPage.js
+++ Frontend/src/page/mainPage.js
@@ -2,13 +2,45 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function MainPage() {
-  console.log("메인화면")
+  console.log("메인화면");
+
+  {/* 테스트 */}
+  const gameInfo = {
+    gameId: "1",
+    name: "여자아이돌 월드컵",
+    description: "나만의 여자아이돌을 뽑아보자"
+  }
+
   return (
     <>
       <h1>메인페이지</h1>
+      <div style={{display:"flex"}}>
       <button>
         <Link to="/make">게임 만들기</Link>
       </button>
+      <button>게임수정하기</button>  {/* 로그인후 보이기 */}
+      </div>
+      {/* 메인화면 스켈레톤 */}
+      <div style={{ width: "222px", height: "391px" }}>
+        <img
+          src={"../images/chungha.jpg"}
+          style={{ width: "111px", height: "180px" }}
+        />
+        <img
+          src={"../images/iu.jpg"}
+          style={{ width: "111px", height: "180px" }}
+        />
+        <div style={{display:"grid"}}>
+        <span style={{fontStyle:"italic"}}>{gameInfo.name}</span>
+        <span>{gameInfo.description}</span>
+        </div>
+        
+        <div >
+       <Link to={`/game/${gameInfo.gameId}`}>게임시작하기</Link>
+        <button>랭킹보기</button>
+        </div>
+        
+      </div>
     </>
   );
 }
