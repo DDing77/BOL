1734a4c948cb9604b6de2d2c4abedc4508c0b412
DDing77 <smg76262585@gmail.com>
2021-11-22 08:22:51 +0900

    Feat: gamePage Skeleton layout

--- Frontend/src/page/gamePage.js
+++ Frontend/src/page/gamePage.js
@@ -1,1 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { startGame } from "../store/actions/game_action";
+
+
+export default function GamePage() {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const gameState = useSelector((store) => store.game);
+//   const path = "../images/";
+
+  console.log(gameState.views[0]);
+  console.log(id);
+  console.log("게임실행 페이지");
+
+  useEffect(() => {
+    dispatch(startGame());
+  }, [gameState.gameId]);
+
+  return (
+    <>
+      <div>
+        <span>{gameState.round}강</span>
+        {gameState.views.length > 0 ? (
+          <div style={{display:"flex"}}>
+            <img style={{width: "300px", height:"450px"}}
+              src={`../images/${gameState.views[0].img}`}
+            />
+            <img style={{width: "300px", height:"450px"}}
+              src={`../images/${gameState.views[1].img}`}
+            />
+          </div>
+        ) : (
+          <div> loading...</div>
+        )}
+      </div>
+    </>
+  );
+}
