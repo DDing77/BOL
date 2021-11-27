074590a55949c3fbdd389b45219084d599f960cd
DDing77 <smg76262585@gmail.com>
2021-11-27 20:18:05 +0900

    Update GameService.js

--- Backend/src/services/GameService.js
+++ Backend/src/services/GameService.js
@@ -14,14 +14,14 @@ export const AddGame = async (req, res, next) => {
     if (!game) {
       return res.send('게임 생성 도중 오류가 발생하였습니다.');
     } 
-
+    let images;
     if(!req.files){
       return res.status(200).send(game);
     } else{
-      images = await req.files.map(element => {
+      images = req.files.map( async (element) =>  {
         console.log("매핑시작");
         console.log(element);
-        ImageRepository.createImage(game.id, element);
+        await ImageRepository.createImage(game.id, element);
       });
     }
 
