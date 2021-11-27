bb5c82992fa19973ef2dde7904093073cc9629c8
DDing77 <smg76262585@gmail.com>
2021-11-27 19:39:08 +0900

    Update GameService.js

--- Backend/src/services/GameService.js
+++ Backend/src/services/GameService.js
@@ -1,19 +1,30 @@
 import fs from 'fs';
 import *as GameRepository from '../repositories/GameRepository';
+import * as ImageRepository from '../repositories/ImageRepository';
 
 export const AddGame = async (req, res, next) => {
   try {
     console.log(req.session.passport);
     console.log(req.body)
+    console.log(req.files)
     const currentUser = req.session.passport.user;
     const game = await GameRepository.createGame(currentUser.id, req.body);
     //게임 업로드 오류발생시.
     console.log(game)
     if (!game) {
       return res.send('게임 생성 도중 오류가 발생하였습니다.');
-    } else {
+    } 
+
+    if(!req.files){
       return res.status(200).send(game);
+    } else{
+      images = await req.files.map(element => {
+        console.log("매핑시작");
+        console.log(element);
+        ImageRepository.createImage(game.id, element);
+      });
     }
+
   } catch (err) {
     console.error(err);
     next(err);
