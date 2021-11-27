d634510e4943248f713f115b738d3894bcd91e04
DDing77 <smg76262585@gmail.com>
2021-11-27 18:16:26 +0900

    feat: ImageHandler(middleware)

--- Backend/src/controllers/GameController.js
+++ Backend/src/controllers/GameController.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import * as AuthHander from '../../middleware/AuthHandler';
+import ImageHandler from '../../middleware/ImageHandler';
 import * as GameService from '../services/GameService';
 const router = express.Router();
 
 router.get('/');
-router.post('/', GameService.AddGame);
+router.post('/', ImageHandler,GameService.AddGame);
 
 export default router;
