f4ec1ee15dccf8210bacb9c409d7c75ae829e0e8
DDing77 <smg76262585@gmail.com>
2021-11-27 09:43:38 +0900

    Add: gameMakePage

--- Frontend/src/page/gameMakePage.js
+++ Frontend/src/page/gameMakePage.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import GameCreateForm from "../components/gameCreateForm";
 export default function GameMakePage() {
+  let navigate = useNavigate();
+
   return (
     <>
-      <h1>게임생성페이지</h1>
+      <h1>게임생성페이지 테스트중</h1>
+      <GameCreateForm/>
     </>
   );
 }
