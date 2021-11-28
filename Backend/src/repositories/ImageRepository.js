bc850d92114fb73685c96896a4b5784ffc07235f
DDing77 <smg76262585@gmail.com>
2021-11-28 11:21:20 +0900

    fix: image.name

--- Backend/src/repositories/ImageRepository.js
+++ Backend/src/repositories/ImageRepository.js
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import path from 'path';
 const prisma = new PrismaClient();
 
 export const createImage = async (gameId, data) => {
   try {
     return await prisma.image.create({
       data: {
-        name: data.originalname,
+        name: path.basename(data.originalname, path.extname(data.originalname)),
         path: data.path,
         game: {
           connect: {
