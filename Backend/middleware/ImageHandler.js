8704636b8f63ab4d22d44e28ea13bfb2faf8605f
DDing77 <smg76262585@gmail.com>
2021-11-28 11:21:08 +0900

    fix: filename

--- Backend/middleware/ImageHandler.js
+++ Backend/middleware/ImageHandler.js
@@ -4,22 +4,11 @@ import path from 'path';
 const storage = multer.diskStorage({
     destination: "./upload",
     filename: (req, file, cb) => {
-      cb(null, Date.now().valueOf() + "_" + path.extname(file.originalname));
+      const ext = path.extname(file.originalname);	// 파일 확장자
+      const timestamp = new Date().getTime().valueOf();	// 현재 시간
+      const filename = path.basename(file.originalname, ext) + timestamp + ext;
+      cb(null, filename);
     }
   })
   
-  const upload = multer({
-    storage: storage,
-    limits: { fileSize: 1000000 }
-  }).array("image");
-  
-
-//   router.post(
-//     "/",
-//     multer({ storage: storage }).array("image"),
-//     (req, res) => {
-//     }
-//   );
-
 export default multer({ storage: storage }).array("image");
-// export const contentUpload = multer({ fieldname: 'images', storage: contentStorage, fileSize: 5 * 1024 * 1024 });
