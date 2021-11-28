faf701dd3ff15961074b99fa75bbbc33ba349fdc
DDing77 <smg76262585@gmail.com>
2021-11-28 11:21:36 +0900

    Refactor: GameService

--- Backend/src/services/GameService.js
+++ Backend/src/services/GameService.js
@@ -1,30 +1,29 @@
 import fs from 'fs';
-import *as GameRepository from '../repositories/GameRepository';
+import * as GameRepository from '../repositories/GameRepository';
 import * as ImageRepository from '../repositories/ImageRepository';
 
 export const AddGame = async (req, res, next) => {
   try {
     console.log(req.session.passport);
-    console.log(req.body)
-    console.log(req.files)
+    console.log(req.body);
+    console.log(req.files);
     const currentUser = req.session.passport.user;
     const game = await GameRepository.createGame(currentUser.id, req.body);
+
     //게임 업로드 오류발생시.
-    console.log(game)
+    console.log(game);
     if (!game) {
       return res.send('게임 생성 도중 오류가 발생하였습니다.');
-    } 
-    let images;
-    if(!req.files){
-      return res.status(200).send(game);
-    } else{
-      images = req.files.map( async (element) =>  {
-        console.log("매핑시작");
-        console.log(element);
-        await ImageRepository.createImage(game.id, element);
-      });
     }
 
+    let images;
+    images =  req.files.map( async element => {
+      console.log('매핑시작');
+      console.log(element);
+      await ImageRepository.createImage(game.id, element);
+    });
+    
+    return res.status(200).send(images);
   } catch (err) {
     console.error(err);
     next(err);
