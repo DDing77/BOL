eb2192e569ec4b765fe2c6c5a1eaa3e2ab2e59b2
DDing77 <smg76262585@gmail.com>
2021-11-29 14:46:13 +0900

    Feat: test getGameComponent

--- Frontend/src/page/mainPage.js
+++ Frontend/src/page/mainPage.js
@@ -1,48 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import GameComponent from "../components/gameComponent";
 
 export default function MainPage() {
-  const allState = useSelector(store=> store);
+  const allState = useSelector((store) => store);
   console.log("메인화면");
   console.log(allState);
-  
- // 테스트 
+
+  // 테스트
   const gameInfo = {
     gameId: "1",
     name: "여자아이돌 월드컵",
-    description: "나만의 여자아이돌을 뽑아보자"
-  }
+    description: "나만의 여자아이돌을 뽑아보자",
+  };
 
   return (
     <>
       <h1>메인페이지</h1>
-      <div style={{display:"flex"}}>
-      <button>
-        <Link to="/make">게임 만들기</Link>
-      </button>
-      <button>게임수정하기</button>  {/* 로그인후 보이기 */}
+      <div style={{ display: "flex" }}>
+        <button>
+          <Link to="/make">게임 만들기</Link>
+        </button>
+        <button>게임수정하기</button> {/* 로그인후 보이기 */}
       </div>
       {/* 메인화면 스켈레톤 */}
       <div style={{ width: "222px", height: "391px" }}>
         <img
+          alt="image1"
           src={"../images/chungha.jpg"}
           style={{ width: "111px", height: "180px" }}
         />
         <img
+          alt="image2"
           src={"../images/iu.jpg"}
           style={{ width: "111px", height: "180px" }}
         />
-        <div style={{display:"grid"}}>
-        <span style={{fontStyle:"italic"}}>{gameInfo.name}</span>
-        <span>{gameInfo.description}</span>
+        <div style={{ display: "grid" }}>
+          <span style={{ fontStyle: "italic" }}>{gameInfo.name}</span>
+          <span>{gameInfo.description}</span>
         </div>
-        
-        <div >
-       <Link to={`/game/${gameInfo.gameId}`}>게임시작하기</Link>
-        <button>랭킹보기</button>
+
+        <div>
+          <Link to={`/game/${gameInfo.gameId}`}>게임시작하기</Link>
+          <button>랭킹보기</button>
         </div>
-        <img src="httP://localhost:5000/upload/Adobe1638066664440.png" style={{width:"300px", height:"300px"}}/>
+        <img
+          alt="testserverimage"
+          src="httP://localhost:5000/upload/Adobe1638066664440.png"
+          style={{ width: "300px", height: "300px" }}
+        />
+      </div>
+      <div>
+        <GameComponent/>
       </div>
     </>
   );
