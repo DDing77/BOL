906904ac27d4f6e93477f4ee3166422f0cc5f42d
DDing77 <smg76262585@gmail.com>
2021-11-29 14:45:27 +0900

    Update getGameInfo.js

--- Frontend/src/util/getGameInfo.js
+++ Frontend/src/util/getGameInfo.js
@@ -1,3 +1,10 @@
-import React from 'react';
-import axios from 'axios';
-import { useDispatch } from "react-redux";
+import React from "react";
+import axios from "axios";
+
+export const getAllGame = async () => {
+  const request =  axios.get("/api/getgames/").then(response => response.data);
+  console.log(request)
+  return await request
+};
+
+export const getGame = (gameId) => {};
