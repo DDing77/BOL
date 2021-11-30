769a5d25e24b035e5808f6284b74c5335d28c4b8
DDing77 <smg76262585@gmail.com>
2021-11-30 18:57:55 +0900

    Feat: Game_Ranking board

--- Frontend/src/page/gameBoardPage.js
+++ Frontend/src/page/gameBoardPage.js
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import * as GetGame from "../util/getGameInfo";
 
 export default function GameBoardPage() {
   const { gameId } = useParams();
+  const [gameInfo, setGameInfo] = useState();
 
-  return <div></div>;
+  useEffect(() => {
+    const get = async () => {
+      const games = await GetGame.getGame(gameId);
+      console.log(games.images);
+      setGameInfo(games);
+    };
+    get();
+  }, []);
+
+  const renderImageInfo = () =>
+    gameInfo.images.map((content, index) => (
+      <tr>
+        <td>{index + 1}</td>
+        <td>
+          <img
+            //   className="img-box"
+            style={{ width: "100px" }}
+            alt={index}
+            src={"httP://localhost:5000/" + `${content.path}`}
+          />
+        </td>
+        <td>{content.name}</td>
+        <td>{content.champion}</td>
+        <td>{content.win}</td>
+      </tr>
+    ));
+
+  return (
+    <div>
+      {gameInfo ? (
+        <div className="container-table">
+          <table>
+            <thead>
+              <tr>
+                <th>순위</th>
+                <th>이미지</th>
+                <th>이름</th>
+                <th>우승비율</th>
+                <th>승률</th>
+              </tr>
+            </thead>
+            <tbody>{renderImageInfo()}</tbody>
+          </table>
+        </div>
+      ) : (
+        "데이터없음"
+      )}
+      <div>
+          댓글
+      </div>
+    </div>
+  );
 }
--- Frontend/src/util/getGameInfo.js
+++ Frontend/src/util/getGameInfo.js
@@ -2,13 +2,19 @@ import React from "react";
 import axios from "axios";
 
 // 모든 게임 정보 가져옴
-export const getAllGame =async () => {
-  const request = await axios.get("/api/games/allgames").then(response => 
-    response.data
-  );
-  console.log(request)
-  return request
+export const getAllGame = async () => {
+  const request = await axios
+    .get("/api/games/allgames")
+    .then((response) => response.data);
+  console.log(request);
+  return request;
 };
 
 // 특정 게임 정보 가져옴
-export const getGame = (gameId) => {};
+export const getGame = async (gameId) => {
+  const request = await axios
+    .get(`/api/games/getgame/${gameId}`)
+    .then((response) => response.data);
+    
+    return request;
+};
