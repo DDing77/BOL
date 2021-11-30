8b86782d804d3f59047b76f46b0c6ce857263171
DDing77 <smg76262585@gmail.com>
2021-11-30 18:26:55 +0900

    Add: gameComponent css

--- Frontend/src/components/gameComponent.js
+++ Frontend/src/components/gameComponent.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import * as GetGame from "../util/getGameInfo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faListOl } from "@fortawesome/free-solid-svg-icons";
+import "../style/gameComponent.css";
 
 export default function GameComponent(userId) {
   //   // 모든 게임 불러오기
@@ -18,30 +21,55 @@ export default function GameComponent(userId) {
 
   const renderAllGameComponent = () =>
     gameInfo.map((content) => (
-      <div style={{ width: "222px", height: "391px" ,padding:"1.7rem"}}>
-        <img
-          alt="image1"
-          src={"httP://localhost:5000/" + `${content.images[0].path}`}
-          style={{ width: "111px", height: "180px" }}
-        />
-        <img
-          alt="image2"
-          src={"httP://localhost:5000/" + `${content.images[1].path}`}
-          style={{ width: "111px", height: "180px" }}
-        />
-        <div style={{ display: "grid" }}>
-          <span style={{ fontStyle: "italic" }}>{content.title}</span>
-          <span>{content.description}({content.images.length}강)</span>
-        </div>
-
-        <div>
-          <Link to={`/game/${content.id}`}>게임시작하기</Link>
-          <button>랭킹보기</button>
+      <div className="gameComponent-container">
+        <div className="gameProduct">
+          <div className="imageSection">
+            <img
+              className="img-box"
+              alt="image1"
+              src={"httP://localhost:5000/" + `${content.images[0].path}`}
+            />
+            <img
+              className="img-box"
+              alt="image2"
+              src={"httP://localhost:5000/" + `${content.images[1].path}`}
+            />
+          </div>
+          <div className="gameProduct-text">
+            <span className="gameProduct-title">{content.title}</span>
+            <span className="gameProduct-description">
+              {content.description}({content.images.length}강)
+            </span>
+          </div>
+          <div className="gameProduct-btn">
+            <button className="btn-start">
+              <Link className="start" to={`/game/${content.id}`}>
+                <FontAwesomeIcon
+                  icon={faPlay}
+                  style={{ paddingRight: "7px" }}
+                />
+                게임시작하기
+              </Link>
+            </button>
+            <button className="btn-rank">
+              <Link className="rank" to={`/rank/${content.id}`} >
+              <FontAwesomeIcon
+                icon={faListOl}
+                style={{ paddingRight: "7px" }}
+              />
+              랭킹보기
+              </Link>
+            </button>
+          </div>
         </div>
       </div>
     ));
 
-  return <div classNamd="gameComponent"style={{display:"flex", flexFlow:"wrap"}}>{gameInfo ? renderAllGameComponent() : "loading"}</div>;
+  return (
+    <div className="gameComponent">
+      {gameInfo ? renderAllGameComponent() : "데이터 없음"}
+    </div>
+  );
 
   // 유저의 게임 불러오기
   //
