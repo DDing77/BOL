653c09c7d1c99618147faa3287e06faf5b0342c2
DDing77 <smg76262585@gmail.com>
2021-12-05 17:31:00 +0900

    Feat: gameCreateFome yup

--- Frontend/src/components/gameCreateForm.js
+++ Frontend/src/components/gameCreateForm.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import *as UploadImage from "../middleware/uploadImage";
+import * as UploadImage from "../middleware/uploadImage";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import "../style/gameCreateForm.css";
 
 export default function GameCreateForm() {
   let navigate = useNavigate();
+
+  const schema = yup.object().shape({
+    title: yup.string().required(),
+    description: yup.string().required(),
+    file: yup
+      .string()
+      .oneOf([yup.ref("password"), null])
+      .required(),
+  });
 
   const {
     register,
@@ -18,25 +30,27 @@ export default function GameCreateForm() {
   };
 
   return (
+    <div className="registerForm-cotainer">
+      <div className="registerForm-title">
+        <span className="registerForm-title"> 이상형 월드컵 만들기</span>
+      </div>
       <form
+        className="registerForm-section"
         onSubmit={handleSubmit(submitForm)}
         encType="multipart/form-data"
-        style={{ display: "flex", flexFlow: "column" }}
       >
-        <label htmlFor="title">게임 타이틀</label>
         <input
           name="title"
           type="text"
           placeholder="게임 타이틀을 입력하세요"
           {...register("title", { required: true, maxLength: 30 })}
         />
-        {errors.name && errors.name.type === "required" && (
-          <span>This is required</span>
-        )}
+        <span style={{ color: "red" }}>
+          {errors.name && "이름 형식이 맞지 않습니다."}
+        </span>
         {errors.name && errors.name.type === "maxLength" && (
           <span>Max length exceeded</span>
         )}
-        <label htmlFor="description">게임 소개</label>
         <input
           name="description"
           type="text"
@@ -58,7 +72,8 @@ export default function GameCreateForm() {
             required: true,
           })}
         />
-        <input type="submit" />
+        <button type="submit">게임 생성</button>
       </form>
+    </div>
   );
 }
