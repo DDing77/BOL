a30c6c7c3d554480be47c4228eaa0a97a9b8c477
DDing77 <smg76262585@gmail.com>
2021-12-09 02:13:57 +0900

    Feat: Get, Delete, Edite Comment API

--- Backend/src/controllers/CommentController.js
+++ Backend/src/controllers/CommentController.js
@@ -4,5 +4,8 @@ import * as CommentService from '../services/CommentService';
 const router = express.Router();
 
 router.post('/',  CommentService.createComment);
+router.get('/:gameid', CommentService.getComments);
+router.post('/delete', CommentService.deleteComment);
+router.post('/edite/:commentid', CommentService.editeComment);
 
 export default router;
--- Backend/src/services/CommentService.js
+++ Backend/src/services/CommentService.js
@@ -1,18 +1,63 @@
 import * as CommentRepository from '../repositories/CommentRepository';
 
 export const createComment = async (req, res, next) => {
-    try{
-        // console.log(req.session.passport);
-        console.log(req.body);
-        // const currentUser = req.session.passport.user;
-        const comment = await CommentRepository.createComment(1,req.body);
+  try {
+    // console.log(req.session.passport);
+    console.log(req.body);
+    // const currentUser = req.session.passport.user;
+    const comment = await CommentRepository.createComment(1, req.body); // 1을 currentUser로 변경
 
-        if(!comment) {
-            return res.send('댓글 생성 도중 오류가 발생하였습니다.');
-        }
-        return res.status(200).send(comment);
-    } catch (err) {
-        console.error(err);
-        next(err);
+    if (!comment) {
+      return res.send('댓글 생성 도중 오류가 발생하였습니다.');
     }
+    return res.status(200).send(comment);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+export const getComments = async (req, res, next) => {
+  try {
+    console.log(req.params);
+    const comments = await CommentRepository.getComments(parseInt(req.params.gameid));
+    if (!comments) {
+      return res.send('댓글 불러오기 실패.');
+    }
+    return res.status(200).send(comments);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+export const deleteComment = async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const result = CommentRepository.deleteComment(parseInt(req.body.commentId));
+    console.log(result);
+    if (!result) {
+      return res.send('댓글 삭제도중 오류 발생.');
+    }
+    return res.status(200).send('댓글 삭제 완료');
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+export const editeComment = async (req, res, next) => {
+  try {
+    console.log(req.body.content);
+    console.log(req.params);
+    const editedComment = CommentRepository.editeComment(parseInt(req.params.commentid), req.body.content);
+    if (!editedComment) {
+      return res.send('댓글 수정 도중 오류 발생.');
+    }
+    console.log(editedComment);
+    return res.status(200).send('댓글 수정 완료');
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 };
