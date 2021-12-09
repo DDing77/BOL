f70595f6dfd6b9ce8d42226be954423368ecaa95
DDing77 <smg76262585@gmail.com>
2021-12-09 09:04:56 +0900

    Feat: isLogin Routing to CommentController, GameController

--- Backend/src/controllers/CommentController.js
+++ Backend/src/controllers/CommentController.js
@@ -3,9 +3,9 @@ import * as AuthHander from '../../middleware/AuthHandler';
 import * as CommentService from '../services/CommentService';
 const router = express.Router();
 
-router.post('/',  CommentService.createComment);
+router.post('/', AuthHander.isLoggined, CommentService.createComment);
 router.get('/:gameid', CommentService.getComments);
-router.post('/delete', CommentService.deleteComment);
-router.post('/edite/:commentid', CommentService.editeComment);
+router.post('/delete', AuthHander.isLoggined, CommentService.deleteComment);
+router.post('/edite/:commentid', AuthHander.isLoggined, CommentService.editeComment);
 
 export default router;
--- Backend/src/controllers/GameController.js
+++ Backend/src/controllers/GameController.js
@@ -4,13 +4,13 @@ import ImageHandler from '../../middleware/ImageHandler';
 import * as GameService from '../services/GameService';
 const router = express.Router();
 
-router.post('/', ImageHandler,GameService.AddGame);
+router.post('/', AuthHander.isLoggined, ImageHandler, GameService.AddGame);
 router.get('/allgames', GameService.getAllGames);
-router.get('/getgame/:gameid', GameService.getOneGame);
-router.get('/getgames/:userid',GameService.getUserGames);
-router.post('/win',GameService.editWin);
-router.post('/champion',GameService.editChampion);
-router.post('/edit', GameService.editGame);
-router.post('/delete',  GameService.deleteGame);
+router.get('/getgame/:gameid', AuthHander.isLoggined, GameService.getOneGame);
+router.get('/getgames/:userid', AuthHander.isLoggined, GameService.getUserGames);
+router.post('/win',AuthHander.isLoggined, GameService.editWin);
+router.post('/champion', AuthHander.isLoggined,GameService.editChampion);
+router.post('/edit', AuthHander.isLoggined,GameService.editGame);
+router.post('/delete',AuthHander.isLoggined, GameService.deleteGame);
 
 export default router;
